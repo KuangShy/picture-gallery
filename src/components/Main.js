e6df8ce2ee10e6550c1094cf2740d053a3f7890f
kuangshuai <kuangshuai@promote.cache-dns.local>
2016-08-22 10:28:57 +0800

    image
    load images

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,24 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+let imageData = require('../data/imageData.json');
+
+function getImageUrl (imageDataArr) {
+  for (let i = 0; i < imageDataArr.length; i++) {
+    let singleImageData = imageDataArr[i];
+    singleImageData.imageUrl = require('../image/singleImageData.fileName');
+    imageDataArr[i] = singleImageData;
+  }
+  return imageDataArr;
+}
+imageData = getImageUrl(imageData);
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
     );
